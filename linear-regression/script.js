0f599fa7b9f7fab99d498f46a60dcec9c9200338
王珏 <wangjuesix@gmail.com>
2020-11-23 11:52:35 +0800

    使用tfjs-vis展示渲染数据

---
+++ linear-regression/script.js
@@ -0,0 +1,14 @@
+import render from "@tensorflow/tfjs-vis"
+
+window.onload = ()=>{
+    const xs = [1,2,3,4]
+    const ys = [1,3,5,7]
+
+    render.scatterplot(
+        {name:'线性回归训练集'},
+        {values: xs.map((x, i)=>{
+            return {x, y:ys[i]}
+        })},
+        {xAxisDomain: [0,5], yAxisDomain: [0,8]}
+    )
+}
