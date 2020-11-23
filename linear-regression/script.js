8b6cc02d9b15c02f902086aa497d879ec4160c65
王珏 <wangjuesix@gmail.com>
2020-11-23 12:16:17 +0800

    创建单层单个神经元神经网络

--- linear-regression/script.js
+++ linear-regression/script.js
@@ -1,14 +1,15 @@
-import render from "@tensorflow/tfjs-vis"
-
+import * as tf from "@tensorflow/tfjs"
 window.onload = ()=>{
     const xs = [1,2,3,4]
     const ys = [1,3,5,7]
-
-    render.scatterplot(
+    tfvis.render.scatterplot(
         {name:'线性回归训练集'},
         {values: xs.map((x, i)=>{
             return {x, y:ys[i]}
         })},
         {xAxisDomain: [0,5], yAxisDomain: [0,8]}
     )
+
+    const model = tf.sequential()
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}))
 }
