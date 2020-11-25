78e5f9a92884c9a99f222a2625b674637d73e1aa
王珏 <wangjuesix@gmail.com>
2020-11-25 11:55:00 +0800

    加载xor数据集

---
+++ xor/index.js
@@ -0,0 +1,15 @@
+import * as tf from "@tensorflow/tfjs"
+import {getData} from "./data"
+
+window.onload = async ()=>{
+    const data = getData(400)
+    tfvis.render.scatterplot(
+        {name: 'XOR训练数据'},
+        {
+            values: [
+                data.filter(p=>p.label === 1),
+                data.filter(p=>p.label === 0)
+            ]
+        }
+    )
+}
