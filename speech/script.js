ee1a1734952860a5697253c72d8185706a0bbdaa
王珏 <wangjuesix@gmail.com>
2020-11-27 14:57:04 +0800

    测试语音识别模型

--- speech/script.js
+++ speech/script.js
@@ -11,6 +11,24 @@ window.onload = async ()=>{
     await recognizer.ensureModelLoaded();
 
     //查看模型可以识别哪些单词
-    const labels = recognizer.wordLabels()
+    const labels = recognizer.wordLabels().slice(2)
     console.log(labels)
+
+    const resultEl = document.querySelector('#result')
+    resultEl.innerHTML = labels.map(l => `
+        <div>${l}</div>
+    `).join('')
+
+    recognizer.listen(result => {
+        console.log("result", result)
+        const { scores } = result;
+        const maxValue = Math.max(...scores);
+        const index = scores.indexOf(maxValue) - 2;
+        resultEl.innerHTML = labels.map((l, i) => `
+        <div style="background: ${i === index && 'green'}">${l}</div>
+        `).join('');
+    }, {
+        overlapFactor: 0.3,
+        probabilityThreshold: 0.9
+    });
 }
